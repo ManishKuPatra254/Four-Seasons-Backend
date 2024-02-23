5fcb4ee8a43857b2ac37ced4cc423d81978ecc03
ManishKuPatra254 <manishpatra254@gmail.com>
2024-02-23 15:51:33 +0530

    setup done

---
+++ config.js
@@ -0,0 +1,35 @@
+const config = {
+    local: {
+        DB: {
+            HOST: "127.0.0.1",
+            PORT: "27017",
+            DATABASE: "fourseasons",
+            UserName: "",
+            Password: ""
+        },
+
+        PORTNO: 8000,
+
+    },
+
+    // staging: {
+    //     DB: {
+    //         HOST: "0.0.0.0",
+    //         PORT: "27017",
+    //         DATABASE: "streamies",
+    //         MONGOOSE: {
+    //             useUndifinedTopology: true,
+    //             useNewUrlParser: true
+    //         },
+    //         UserName: "manishpatra254",
+    //         Password: "pbXAlfYZ5LgNbfje"
+    //     },
+
+
+    //     PORTNO: 8680,
+
+    // },
+}
+export const get = function get(env) {
+    return config[env];
+}
