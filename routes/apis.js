51f61343f966ff34bddca743598e031b383a8813
ManishKuPatra254 <manishpatra254@gmail.com>
2024-02-27 16:19:35 +0530

    added getbyId apis

--- routes/apis.js
+++ routes/apis.js
@@ -1,9 +1,13 @@
 var express = require('express');
-const { userFormSignup, userFormLogin } = require('../controller/apis');
+const { userFormSignup, userFormLogin, getUserById } = require('../controller/apis');
 var router = express.Router();
 
 
 router.post('/signupuser', userFormSignup),
   router.post('/loginuser', userFormLogin),
+  router.get('/getUserById/:id', getUserById),
+
+  // router.get('/getDataById/:id', getUserById);
+
 
   module.exports = router;
